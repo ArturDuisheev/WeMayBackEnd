6e58288ecddb9525318bd0c9470ddb462c6431cb
BreadDev <breadydev@gmail.com>
2024-03-01 19:51:10 +0600

    Swagger schema title changed

--- core/urls.py
+++ core/urls.py
@@ -27,7 +27,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Library",
+        title="WeMay",
         default_version='v1',
         description="Swagger",
         terms_of_service="https://www.example.com/terms/",
