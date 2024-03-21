0e924454e29232ee574ee35f41ac9622b92ae9ab
GGduisheevArturGG <107888994+ArturDuisheev@users.noreply.github.com>
2024-03-21 16:30:18 +0600

    add id

--- src/promotion/admin.py
+++ src/promotion/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Contact, PromotionCategory, Promotion
+from .models import Contact, PromotionCategory, Promotion, PromotionAddress
 # Register your models here.
 
 
 admin.site.register(PromotionCategory)
 admin.site.register(Promotion)
+admin.site.register(PromotionAddress)
 admin.site.register(Contact)
 admin.site.register(Promotion.likes.through)
