02ea7dd2fbe2e66220e96f472052c4e4499745d3
ArturDuisheev <myworkingartur@gmail.com>
2024-05-11 19:42:50 +0600

    add author

--- src/review/api/serializers.py
+++ src/review/api/serializers.py
@@ -4,13 +4,31 @@ from review.models import Review
 
 class ReviewSerializer(serializers.ModelSerializer):
     created_time = serializers.ReadOnlyField()
+    author = serializers.SerializerMethodField('get_author')
 
     class Meta:
         model = Review
-        fields = '__all__'
+        fields = (
+            'author',
+            'promotion',
+            'body',
+            'likes',
+            'created_time'
+        )
 
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['author_username'] = instance.author.username
-        representation['promotion_title'] = instance.promotion.title
-        return representation
+    def get_author(self, obj):
+        request = self.context.get('request')
+        image = obj.author.image
+        return {
+            'username': obj.author.username,
+            'image': request.build_absolute_uri(image)
+        }
+
+
+
+
+    # def to_representation(self, instance):
+    #     representation = super().to_representation(instance)
+    #     representation['author_username'] = instance.author.username
+    #     representation['promotion_title'] = instance.promotion.title
+    #     return representation
